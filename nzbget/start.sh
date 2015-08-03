44c196a50d217463fc3e79c19cc85057f00e8afb
Michael Mansell <michael.mansell@gmail.com>
2015-08-03 10:26:18 -0400

    Updated Sonarr and Nzbget

--- nzbget/start.sh
+++ nzbget/start.sh
@@ -3,5 +3,7 @@
 set -e
 
 [ ! -f /config/nzbget.conf ] && cp /usr/local/share/nzbget/nzbget.conf /config/nzbget.conf
+[ ! -f /config/nzbget.log ] && touch /config/nzbget.log
 
 nzbget --configfile /config/nzbget.conf --daemon
+tail -f /config/nzbget.log
